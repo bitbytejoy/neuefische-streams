c5fd81e5a22bd780709ea6b751ff66e14c9bf21b
bitbytejoy <bitbytejoy@gmail.com>
2022-12-06 14:34:39 +0100

    feat: Aufgabe

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,10 +1,13 @@
 package de.neuefische;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> words = new ArrayList<>(
             List.of(
                 "cat",
@@ -82,5 +85,87 @@ public class Main {
         System.out.println();
         System.out.println("Sorted");
         System.out.println(words.stream().sorted().collect(Collectors.toList()));
+
+        int[] zahlen = new int[]{1, 2, 3, 4};
+        System.out.println("Sum: " + sum(zahlen));
+        System.out.println("Product: " + product(zahlen));
+
+        System.out.println(parseStudentsCSV());
+        System.out.println(parseStudentsCSVFor());
+    }
+
+    public static int sum (int[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+
+    public static int product (int[] numbers) {
+        return Arrays.stream(numbers)
+                .reduce((number, sum) -> sum * number)
+                .orElse(0);
+    }
+
+    // ["1,Stefan,12345,40", "1,Stefan,12345,40"]
+    // [["1", "Stefan", "12345", "40"], [], ["1", "Stefan", "12345", "40"]]
+    // [["1", "Stefan", "12345", "40"], ["Abc", "Stefan", "12345", "40"]]
+    // [["1", "Stefan", "12345", "40"]]
+    // [Student[id=1,name='Stefan',postalCode='12345',age=40]]
+    public static List<Student> parseStudentsCSV () throws IOException {
+        return Files.lines(
+                Path.of(
+                    "/Users/bitbytejoy/Downloads/neuefische-streams/src/main/resources/students.csv"
+                )
+            ).skip(1)
+            .map(line -> line.split(","))
+            .filter(properties -> properties.length == 4)
+            .filter(properties -> {
+                try {
+                    Integer.parseInt(properties[0]);
+                    Integer.parseInt(properties[3]);
+                    return true;
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            })
+            .map(properties -> new Student(
+                Integer.parseInt(properties[0]),
+                properties[1],
+                properties[2],
+                Integer.parseInt(properties[3])
+            )).distinct()
+            .collect(Collectors.toList());
+    }
+
+    public static List<Student> parseStudentsCSVFor () throws IOException {
+        List<String> lines = Files.readAllLines(
+            Path.of(
+            "/Users/bitbytejoy/Downloads/neuefische-streams/src/main/resources/students.csv"
+            )
+        );
+
+        Set<Student> studentsUnique = new HashSet<>();
+
+        for (String line : lines) {
+            List<String> properties = List.of(line.split(","));
+
+            if (properties.size() != 4) {
+                continue;
+            }
+
+            try {
+                Integer.parseInt(properties.get(0));
+                Integer.parseInt(properties.get(3));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+            studentsUnique.add(new Student(
+                Integer.parseInt(properties.get(0)),
+                properties.get(1),
+                properties.get(2),
+                Integer.parseInt(properties.get(3))
+            ));
+        }
+
+        return new ArrayList<>(studentsUnique);
     }
 }
---
+++ src/main/java/de/neuefische/Student.java
@@ -0,0 +1,13 @@
+package de.neuefische;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+    private int id;
+    private String name;
+    private String postalCode;
+    private int age;
+}
